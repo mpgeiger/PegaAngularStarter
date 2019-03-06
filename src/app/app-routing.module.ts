c2d699c13de9cff64ad2661bd776b4b32cbadd19
Geiger <Matt.Geiger@pega.com>
2019-03-06 15:15:02 -0500

    Added multiple NAV buttons working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WorklistComponent } from './worklist/worklist.component';
 import { MPGLayoutComponent } from './mpg-layout/mpg-layout.component';
+import { AccountsComponent } from './accounts/accounts.component';
+
 
 
 const routes: Routes = [
@@ -12,7 +14,11 @@ const routes: Routes = [
   {
     path: 'mpg-layout',
     component: MPGLayoutComponent,
-  }
+  },
+  {
+    path: 'accounts',
+    component: AccountsComponent,
+  },
 
 
 ];
