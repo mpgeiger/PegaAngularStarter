ea895d59c008c9febcddc7c0431a5afcd71ba5c5
Geiger <Matt.Geiger@pega.com>
2019-03-12 18:12:36 -0500

    modified 127.0.0.1 to end2endcrmcommercialbanking.pegatsdemo.com

--- src/app/_services/endpoints.ts
+++ src/app/_services/endpoints.ts
@@ -1,7 +1,7 @@
 export const endpoints = {
   // Change this URL if you want to point the React application at another Pega server .
   // BASEURL: "http://172.17.130.3:9080/prweb/api/v1",
-  BASEURL: "https://127.0.0.1/prweb/api/v1",
+  BASEURL: "https://end2endcrmcommercialbanking.pegatsdemo.com/prweb/api/v1",
   AUTH: "/authenticate",
   CASES: "/cases",
   CASETYPES: "/casetypes",
