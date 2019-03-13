fe234242572694fdf297c8ae85df517d6fff9ba6
Geiger <Matt.Geiger@pega.com>
2019-03-13 06:47:04 -0500

    added route for /

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     component: WorklistComponent,
   },
   {
+    path: '',
+    component: MaintabsComponent,
+  },
+  {
     path: 'maintabs',
     component: MaintabsComponent,
   },
